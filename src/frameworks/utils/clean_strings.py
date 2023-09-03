2a24cbc9718ca31944f9f1c4d31f05cc021b7968
NicoBetancourt <jnbetancourtl@gmail.com>
2023-09-03 12:12:48 +0200

    feat: Se crea utilidad para limpiar cadenas de texto

--- src/frameworks/utils/clean_strings.py
+++ src/frameworks/utils/clean_strings.py
@@ -1,14 +1,8 @@
 import re
 
 def limpiar_cadena(cadena):
-    # Eliminar espacios iniciales y finales
-    # cadena = cadena.strip()
 
-    # # Utilizar una expresión regular para eliminar números, guiones y porcentaje dentro del paréntesis
-    # patron = r'\(\d+\.\d+%?\)'
-    # cadena = re.sub(patron, '', cadena)
-
-    # Luego eliminar cualquier otro carácter que no sea una letra o un espacio
+    # Se eliminan espacios y caracteres especiales
     cadena = re.sub(r'[^a-zA-Z\s]', '', cadena).strip()
 
     return cadena
